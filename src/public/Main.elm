4463f3e58a25aa37591ac48aa02fbc93aeb48393
Sam Houston <shouston3@me.com>
2017-02-13 17:55:20 +0000

    reaching a state of being able to send a http request

--- src/public/Main.elm
+++ src/public/Main.elm
@@ -1,63 +1,12 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
+import String
 
-
--- MODEL
-
-
-type alias Model =
-    String
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( "Hello", Cmd.none )
-
-
-
--- MESSAGES
-
-
-type Msg
-    = NoOp
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
-
-
--- MAIN
-
-
-main : Program Never Model Msg
 main =
     program
         { init = init
@@ -65,3 +14,54 @@ main =
         , update = update
         , subscriptions = subscriptions
         }
+
+type alias Model = {
+  repos : List Entry
+}
+
+type alias Entry = String
+
+init : ( Model, Cmd Msg )
+init =
+    ( { repos = []
+      }
+    , Cmd.none
+    )
+
+type Msg
+    = HandleRepos
+    | GetRepos (Result Http.Error String)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+      HandleRepos ->
+        (model, getRepos)
+      GetRepos (Ok repos) ->
+          (Model model.repos, Cmd.none)
+      GetRepos (Err _) ->
+          (model, Cmd.none)
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ p [] [ text (toString model.repos) ]
+        , button [ onClick HandleRepos ] [ text "Welcome" ]
+        ]
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+getRepos : Cmd Msg
+getRepos =
+  let
+    url =
+      "/repos/dwyl"
+  in
+    Http.send GetRepos (Http.get url decodeRepoUrl)
+
+decodeRepoUrl : Decode.Decoder String
+decodeRepoUrl =
+  Decode.at [ "response" ] Decode.string
+
